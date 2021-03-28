0e5f88ce50c7487713ccb4eace7a91d730f15603
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2021-03-28 04:55:36 -0300

    ✨ Esquema de model ativável

--- pizzaria/modules/core/models.py
+++ pizzaria/modules/core/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+from .managers import ActivableManager
+
+class ActivableModel(models.Model):
+    ativo = models.BooleanField(default=True)
+    objects = ActivableManager()
