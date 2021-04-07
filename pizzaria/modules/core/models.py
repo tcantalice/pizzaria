7acf936765b0941378bb077a2c49f76a2beb9d53
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2021-04-07 01:05:12 -0300

    🐛 Corrigida falha de label das models do módulo 'core'

--- pizzaria/modules/core/models.py
+++ pizzaria/modules/core/models.py
@@ -6,5 +6,11 @@ class ActivableModel(models.Model):
     ativo = models.BooleanField(default=True)
     objects = ActivableManager()
 
+    class Meta:
+        app_label = 'core'
+
 class DomainModel(models.Model):
     descricao = models.CharField(max_length=45)
+
+    class Meta:
+        app_label = 'core'
