c3914c846d876ad45561b38c4ce877755792b432
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2021-04-07 21:07:19 -0300

    ♻️ Sobreescrito construtor das configurações do módulo
    Implementado método estático para obter o label do módulo
    Implementada classe base de metadados para o módulo 'core'

--- pizzaria/modules/core/config.py
+++ pizzaria/modules/core/config.py
@@ -4,9 +4,16 @@ from pizzaria.bootstrap import handler_module_ready as init
 
 class CoreConfig(AppConfig):
     name = 'pizzaria.modules.core'
-    label = 'core'
     verbose_name = 'Pizzaria - Módulo Core'
+
+    def __init__(self, app_name, app_module):
+        super().__init__(app_name, app_module)
+        self.label = self.__class__.module_label()
 
     @init
     def ready(self):
         return
+
+    @staticmethod
+    def module_label():
+        return 'core'
