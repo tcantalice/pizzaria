b16d784c1b86f59f67ddc90892a69dff156b8444
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2021-04-08 01:34:54 -0300

    ♻️ Adicionada opção de abstract às classes Meta
    Adicionada opção 'ordering' à Meta de DomainModel

--- pizzaria/modules/core/models.py
+++ pizzaria/modules/core/models.py
@@ -3,17 +3,22 @@ from django.db import models
 from .managers import ActivableManager
 from .config import CoreConfig
 
+
 class _Meta:
     app_label = CoreConfig.module_label()
+
 
 class ActivableModel(models.Model):
     ativo = models.BooleanField(default=True)
     objects = ActivableManager()
 
     class Meta(_Meta):
-        ...
+        abstract = True
+
+
 class DomainModel(models.Model):
     descricao = models.CharField(max_length=45)
 
     class Meta(_Meta):
-        ...
+        abstract = True
+        ordering = ('descricao',)
