a575337b62bcae6820a4cba2801a097723942778
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2021-04-08 17:12:48 -0300

    Definidas configurações do módulo de produtos

--- pizzaria/modules/product/config.py
+++ pizzaria/modules/product/config.py
@@ -1,5 +1,20 @@
-from django.apps import AppConfig
+from pizzaria.modules.core.config import ModuleConfig
+from pizzaria.bootstrap import (
+    handler_module_ready as init,
+    check_module_dependencies as check_dependencies
+)
 
 
-class ProductConfig(AppConfig):
-    name = 'product'
+class ProductConfig(ModuleConfig):
+    name = 'pizzaria.modules.product'
+    verbose_name = 'Pizzaria - Módulo de Produtos'
+
+    dependencies = ('pizzaria.modules.core',)
+
+    @init
+    def ready(self):
+        check_dependencies(self.name, self.dependencies)
+
+    @staticmethod
+    def module_label():
+        return 'product'
