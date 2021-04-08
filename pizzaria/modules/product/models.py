60dc830a799178fcaf57dbf020dcc358d83950a5
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2021-04-08 19:48:34 -0300

    ✨ Implementada model de sabor

--- pizzaria/modules/product/models.py
+++ pizzaria/modules/product/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from pizzaria.modules.core.models import ActivableModel
+from pizzaria.modules.domain.models import TipoSabor, Ingrediente
+
+from .config import ProductConfig
+
+class _Meta:
+    app_label = ProductConfig.module_label()
+
+
+class Sabor(ActivableModel):
+    descricao = models.CharField(max_length=45)
+    tipo = models.ForeignKey(TipoSabor, on_delete=models.RESTRICT)
+    ingredientes = models.ManyToManyField(Ingrediente, db_table='sabor_ingrediente')
+
+    class Meta(_Meta, ActivableModel.Meta):
+        db_table = 'sabor'
+
