c44c01ae748cf339706478658a830cd5c9ab30ae
Tarcisio Cantalice <tarcisiocantalice@gmail.com>
2021-04-07 22:04:43 -0300

    Implementada classe base de configurações de módulos
    Configurações dos módulos 'core' e 'domain' extendendo de ModuleConfig

--- pizzaria/modules/domain/config.py
+++ pizzaria/modules/domain/config.py
@@ -1,14 +1,14 @@
-from django.apps import AppConfig, registry
+from django.apps import registry
 
+from pizzaria.modules.core.config import ModuleConfig
 from pizzaria.bootstrap import (
     handler_module_ready as init,
-    check_module_dependencies as check_dependencies,
+    check_module_dependencies as check_dependencies
 )
 
 
-class DomainConfig(AppConfig):
+class DomainConfig(ModuleConfig):
     name = 'pizzaria.modules.domain'
-    label = 'domain'
     verbose_name = 'Pizzaria - Módulo de Domínio'
 
     dependencies = ('pizzaria.modules.core',)
@@ -16,3 +16,8 @@ class DomainConfig(AppConfig):
     @init
     def ready(self):
         check_dependencies(self.name, self.dependencies, registry)
+
+    @staticmethod
+    def module_label():
+        return 'domain'
+
